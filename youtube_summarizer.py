b8293f1bdd404f404d0d115344c9b3909f857d9c
Roopam-kapoor <65160331+Roopam-kapoor@users.noreply.github.com>
2025-06-04 21:58:25 +0530

    Update youtube_summarizer.py

--- youtube_summarizer.py
+++ youtube_summarizer.py
@@ -30,8 +30,8 @@ def extract_video_id(url):
 
 # Proxy setup
 proxies = {
-    "http": "http://nxpgclyy:v6vl9y7d2zkl@proxy-ip:6540",
-    "https": "http://nxpgclyy:v6vl9y7d2zkl@proxy-ip:6540"
+    "http": "http://nxpgclyy:v6vl9y7d2zkl@198.23.239.134:6540",
+    "https": "http://nxpgclyy:v6vl9y7d2zkl@198.23.239.134:6540"
 }
 os.environ['http_proxy'] = proxies['http']
 os.environ['https_proxy'] = proxies['https']
